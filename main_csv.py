ae37d5d88bfdaf864db05508d34d8f1394ba15b4
Kraken0666 <ls.kraken0666@protonmail.com>
2024-10-29 19:03:03 +0000

    Improved handling of non-reachable Content IDs

--- main_csv.py
+++ main_csv.py
@@ -2,14 +2,20 @@ import requests
 import csv
 import json
 import os
+from pathlib import Path
 
+#Empty placeholder values for url as they need to exist before they can be replaced within a for-loop
 language = ""
 country = ""
 
-with open("lbp.csv", "r") as file:
+
+counter=0
+
+with open("csv/lbp.csv", "r") as file:
     csvFile = csv.reader(file)
     next(csvFile)
     for entries in csvFile:
+        counter+=1
         contentID = entries[1]
         serviceIDPrefix = contentID[0:6]
         npTitleID = contentID[7:19]
@@ -36,72 +42,55 @@ with open("lbp.csv", "r") as file:
             + contentID
         )
 
-        # Create folder for files to be saved to
-        os.makedirs(
-            "downloads/" + str(npTitleID) + "/" + str(entitlementID), exist_ok=True
-        )
-
         # Request data from API
         response = requests.get(url)
         data = response.json()
 
         # Save the data requested from API
         try:
-            # Show current Content ID
-            print(data["id"] + " | " + data["name"])
+            if Path("downloads/" + str(npTitleID) + "/" + str(entitlementID)).is_dir() == True:
+                try:
+                    print(str(counter) + " | " + str(contentID) + " | " + data["name"] + " | Already exists")
+                except:
+                    print(str(counter) + " | No available data")
+                    continue
+            else:
+                #Create folders for entitlement ID that do not currently exist
+                os.makedirs(
+                    "downloads/"
+                    + str(npTitleID)
+                    + "/" 
+                    + str(entitlementID),
+                    exist_ok=True
+                    )
 
-            # Output JSON data to file
-            json_str = json.dumps(data, indent=4)
-            with open(
-                "downloads/"
-                + npTitleID
-                + "/"
-                + entitlementID
-                + "/"
-                + data["id"]
-                + ".json",
-                "w",
-            ) as file:
-                print("Saving... " + data["id"] + ".json")
-                file.write(json_str)
+                # Show current Content ID
+                print(str(counter) 
+                + " | " 
+                + data["id"] 
+                + " | " 
+                + data["name"]
+                )
 
-            # Output icon to file(s)
-            ## Save icons extracted from JSON
-            for i in data["images"]:
-                if isinstance(i, dict):
-                    for k, v in i.items():
-                        if k == "url":
-                            response = requests.get(v).content
-                            with open(
-                                "downloads/"
-                                + npTitleID
-                                + "/"
-                                + entitlementID
-                                + "/"
-                                + str(v[58:]),
-                                "wb",
-                            ) as file:
-                                print("Saving... " + str(v[58:]))
-                                file.write(response)
+                # Output JSON data to file
+                json_str = json.dumps(data, indent=4)
+                with open(
+                    "downloads/"
+                    + npTitleID
+                    + "/"
+                    + entitlementID
+                    + "/"
+                    + data["id"]
+                    + ".json",
+                    "w",
+                ) as file:
+                    print("Saving... " + data["id"] + ".json")
+                    file.write(json_str)
+                file.close()
 
-            ## Save icon from official API /image endpoint
-            response = requests.get(url + "/image").content
-            with open(
-                "downloads/"
-                + npTitleID
-                + "/"
-                + entitlementID
-                + "/"
-                + data["id"]
-                + ".jpg",
-                "wb",
-            ) as file:
-                print("Saving... " + data["id"] + ".jpg")
-                file.write(response)
-
-            ## Save promomedia extracted from JSON
-            try:
-                for i in data["promomedia"][0]["materials"][0]["urls"]:
+                # Output icon to file(s)
+                ## Save icons extracted from JSON
+                for i in data["images"]:
                     if isinstance(i, dict):
                         for k, v in i.items():
                             if k == "url":
@@ -112,18 +101,56 @@ with open("lbp.csv", "r") as file:
                                     + "/"
                                     + entitlementID
                                     + "/"
-                                    + str(v[49:]),
+                                    + str(v[58:]),
                                     "wb",
                                 ) as file:
-                                    print("Saving... " + str(v[49:]))
+                                    print("Saving... " + str(v[58:]))
                                     file.write(response)
-            except:
-                pass
+                                file.close()
 
-            print("\n", end="")
+                ## Save icon from official API /image endpoint
+                response = requests.get(url + "/image").content
+                with open(
+                    "downloads/"
+                    + npTitleID
+                    + "/"
+                    + entitlementID
+                    + "/"
+                    + data["id"]
+                    + ".jpg",
+                    "wb",
+                ) as file:
+                    print("Saving... " + data["id"] + ".jpg")
+                    file.write(response)
+                file.close()
+
+                ## Save promomedia extracted from JSON
+                try:
+                    for i in data["promomedia"][0]["materials"][0]["urls"]:
+                        if isinstance(i, dict):
+                            for k, v in i.items():
+                                if k == "url":
+                                    response = requests.get(v).content
+                                    with open(
+                                        "downloads/"
+                                        + npTitleID
+                                        + "/"
+                                        + entitlementID
+                                        + "/"
+                                        + str(v[49:]),
+                                        "wb",
+                                    ) as file:
+                                        print("Saving... " + str(v[49:]))
+                                        file.write(response)
+                                    file.close()
+                except:
+                    pass
+
+                print("\n", end="")
+                counter+=1
 
         except:
             os.rmdir("downloads/" + npTitleID + "/" + entitlementID)
-            with open("downloads/" + npTitleID + "/" + "errlog.log", "a") as file:
-                file.write(url)
+            with open("downloads/" + "errlog.log", "a") as file:
+                file.write(url+"\n")
             file.close()
